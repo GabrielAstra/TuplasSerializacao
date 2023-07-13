72bf4f968a215d151e434d7edc7325db3380c2b9
GabrielAstra <gvlc2013@gmail.com>
2023-07-13 12:21:08 -0300

    Tuplas

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using Tuplas.Models;
+
+
+    LeituraArquivo arquivoLer = new LeituraArquivo();
+
+    var (Sucesso, linhas, QuantidadeLinhas) = arquivoLer.LerArquivo("ArquivoTexto/Leitura.txt");
+
+    if (Sucesso)
+    {
+        Console.WriteLine("Foi possível ler o arquivo: ");
+        foreach (string linha in linhas)
+        {
+            Console.WriteLine(linha);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Erro: Não foi possível ler o arquivo");
+    }
+
+    Console.WriteLine("-------------------");
+
+    string json = JsonConvert.SerializeObject(arquivoLer);
+    Console.WriteLine(json);
+    File.WriteAllText("ArquivoTexto/chazon.json", json);
+
+
+// Use o "_" para descartar uma variável.
